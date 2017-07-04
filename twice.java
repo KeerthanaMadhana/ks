0e1b0b1abe74370e6c017c0c2ed7c8ee37d05c5e
KeerthanaMadhana <keerthana19963071@gmail.com>
2017-07-04 15:16:38 +0530

    Create twice.java

---
+++ twice.java
@@ -0,0 +1,14 @@
+package ks;
+import java.util.*;
+public class Twice {
+	public static void main(String[] args) {
+		int r=0;
+		int a[]={12, 1, 12, 3, 12, 1, 1, 2, 3, 2, 2, 3, 7};
+		for(int i=0;i< a.length;i++)
+		{
+		r=a[i]|r%3;
+		}
+		System.out.println(r);
+		}
+}
+
